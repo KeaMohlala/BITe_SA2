f263d350f8926355fa468d4c88e6b9ae19a5cf3a
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2024-06-21 16:10:14 +0200

    intergrate edamam API

--- src/script.js
+++ src/script.js
@@ -5,13 +5,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
   form.addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent the form from submitting normally
-    var ingredient = input.value.trim(); // Get the trimmed input value
-    if (ingredient) {
-      // Check if the input is not empty
-      resultsDiv.innerHTML += `<p>${ingredient}</p>`; // Display the input value
-      input.value = ""; // Clear the input field
+    var ingredients = input.value
+      .trim()
+      .split(",")
+      .map((i) => i.trim()); // Assuming ingredients are comma-separated
+    if (ingredients.length > 0) {
+      fetchRecipes(ingredients.join());
     } else {
-      alert("Please enter an ingredient.");
+      alert("Please enter ingredients.");
     }
+    input.value = ""; // Clear the input field
   });
+
+  function fetchRecipes(query) {
+    var appId = "21ae8994";
+    var appKey = "7578b2b129cbd412347cd9e03a326232";
+    var url = `https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(
+      query
+    )}&app_id=${appId}&app_key=${appKey}%09&imageSize=THUMBNAIL`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        resultsDiv.innerHTML = ""; // Clear previous results
+        data.hits.forEach((hit) => {
+          var resultElement = document.createElement("div");
+          resultElement.textContent = hit.recipe.label; // Display recipe label
+          resultsDiv.appendChild(resultElement);
+        });
+      })
+      .catch((error) => console.error("Error fetching recipes:", error));
+  }
 });
