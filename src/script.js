fb01e2ebae9a24ede278279fdc424359eab3f1a8
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2024-06-23 22:04:51 +0200

    load recipes on more than one page

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var mealTypeFilter = document.getElementById("mealTypeFilter");
   var cuisineTypeFilter = document.getElementById("cuisineTypeFilter");
   var dietFilter = document.getElementById("dietFilter");
+  var recipesButton = document.getElementById("moreRecipesButtonContainer");
 
   form.addEventListener("submit", function (e) {
     e.preventDefault(); // Prevent the form from submitting normally
@@ -25,6 +26,92 @@ document.addEventListener("DOMContentLoaded", function () {
     //input.value = ""; // Clear the input field
   });
 
+  function fetchNextPage(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // Process and display recipes
+        data.hits.forEach((hit) => {
+          var recipeCard = document.createElement("div");
+          recipeCard.className = "col-md-6 card";
+
+          var recipeImage = document.createElement("img");
+          recipeImage.src = hit.recipe.image;
+          recipeImage.alt = hit.recipe.label;
+
+          var recipeTitle = document.createElement("h5");
+          recipeTitle.className = "recipe-title";
+          recipeTitle.textContent = hit.recipe.label;
+
+          var recipeTime = document.createElement("p");
+          recipeTime.className = "recipe-time";
+          recipeTime.textContent = `Cooking Time: ${hit.recipe.totalTime} minutes`;
+
+          var recipeLink = document.createElement("a");
+          recipeLink.href = hit.recipe.url;
+          recipeLink.target = "_blank";
+          recipeLink.textContent = "Full Recipe";
+
+          recipeCard.appendChild(recipeImage);
+          recipeCard.appendChild(recipeTitle);
+          recipeCard.appendChild(recipeTime);
+          recipeCard.appendChild(recipeLink);
+
+          resultsContainer.appendChild(recipeCard);
+        });
+        var nextpageButton = document.createElement("button");
+        nextpageButton.textContent = "More Recipes";
+        nextpageButton.onclick = function () {
+          fetchNextPage(data._links.next.href);
+        };
+        recipesButton.innerHTML = "";
+        recipesButton.appendChild(nextpageButton);
+        if (data._links.next.href) {
+          nextpageButton.style.display = "block";
+        } else {
+          nextpageButton.style.display = "none";
+        }
+      })
+      .catch((error) => console.error("Error fetching next page:", error));
+  }
+
+  function fetchPage(url) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // Process and display recipes
+        data.hits.forEach((hit) => {
+          var recipeCard = document.createElement("div");
+          recipeCard.className = "col-md-6 card";
+
+          var recipeImage = document.createElement("img");
+          recipeImage.src = hit.recipe.image;
+          recipeImage.alt = hit.recipe.label;
+
+          var recipeTitle = document.createElement("h5");
+          recipeTitle.className = "recipe-title";
+          recipeTitle.textContent = hit.recipe.label;
+
+          var recipeTime = document.createElement("p");
+          recipeTime.className = "recipe-time";
+          recipeTime.textContent = `Cooking Time: ${hit.recipe.totalTime} minutes`;
+
+          var recipeLink = document.createElement("a");
+          recipeLink.href = hit.recipe.url;
+          recipeLink.target = "_blank";
+          recipeLink.textContent = "Full Recipe";
+
+          recipeCard.appendChild(recipeImage);
+          recipeCard.appendChild(recipeTitle);
+          recipeCard.appendChild(recipeTime);
+          recipeCard.appendChild(recipeLink);
+
+          resultsContainer.appendChild(recipeCard);
+        });
+      })
+      .catch((error) => console.error("Error fetching page:", error));
+  }
+
   function fetchRecipes(query, mealType, cuisineType, diet) {
     var appId = "21ae8994";
     var appKey = "7578b2b129cbd412347cd9e03a326232";
@@ -43,37 +130,13 @@ document.addEventListener("DOMContentLoaded", function () {
           resultsContainer.innerHTML =
             "<h5>No recipes found matching your criteria.</h5>";
         } else {
-          // Process and display recipes
           var recipeCount = data.count;
           resultsContainer.innerHTML = `<h5> ${recipeCount} Recipes Found </h5>`;
-          data.hits.forEach((hit) => {
-            var recipeCard = document.createElement("div");
-            recipeCard.className = "col-md-6 card";
-
-            var recipeImage = document.createElement("img");
-            recipeImage.src = hit.recipe.image;
-            recipeImage.alt = hit.recipe.label;
-
-            var recipeTitle = document.createElement("h5");
-            recipeTitle.className = "recipe-title";
-            recipeTitle.textContent = hit.recipe.label;
-
-            var recipeTime = document.createElement("p");
-            recipeTime.className = "recipe-time";
-            recipeTime.textContent = `Cooking Time: ${hit.recipe.totalTime} minutes`;
-
-            var recipeLink = document.createElement("a");
-            recipeLink.href = hit.recipe.url;
-            recipeLink.target = "_blank";
-            recipeLink.textContent = "Full Recipe";
-
-            recipeCard.appendChild(recipeImage);
-            recipeCard.appendChild(recipeTitle);
-            recipeCard.appendChild(recipeTime);
-            recipeCard.appendChild(recipeLink);
-
-            resultsContainer.appendChild(recipeCard);
-          });
+          if (data._links.next) {
+            fetchNextPage(data._links.next.href);
+          } else {
+            fetchPage(url);
+          }
         }
       })
       .catch((error) => console.error("Error fetching recipes:", error));
